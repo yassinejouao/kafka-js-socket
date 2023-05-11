60d24dd2a44aed77628aa4273ea91a5c199d120b
y.jouaoudi <y.jouaoudi@opentech.ma>
2023-05-11 16:57:14 +0100

    add runallprodbat

---
+++ producer/runallprod.bat
@@ -0,0 +1,12 @@
+@echo off
+
+start cmd /k "python.exe .\prod1.py"
+start cmd /k "python.exe .\prod2.py"
+start cmd /k "python.exe .\prod3.py"
+start cmd /k "python.exe .\prod4.py"
+start cmd /k "python.exe .\prod5.py"
+start cmd /k "python.exe .\prod6.py"
+start cmd /k "python.exe .\prod7.py"
+start cmd /k "python.exe .\prod8.py"
+start cmd /k "python.exe .\prod9.py"
+start cmd /k "python.exe .\prod10.py"
